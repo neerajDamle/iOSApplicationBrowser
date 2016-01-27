f24992d2260ada78504539a13cd424f30f31c2a1
neeraj.damle <neeraj.damle@springcomputing.in>
2016-01-27 19:11:37 +0530

    Added empty project

---
+++ iOSAppBrowser/iOSAppBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSAppBrowser
+//
+//  Created by Neeraj Damle on 1/27/16.
+//  Copyright © 2016 Neeraj Damle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSAppBrowser/iOSAppBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSAppBrowser
+//
+//  Created by Neeraj Damle on 1/27/16.
+//  Copyright © 2016 Neeraj Damle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
